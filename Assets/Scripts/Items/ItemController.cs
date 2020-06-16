1a02568fe8f0e93efa52ecdac9efdeaedf5d91e4
Cooper Saye <coopersaye@gmail.com>
2020-06-16 12:15:55 -0700

    Add hotbar UI.

---
+++ Assets/Scripts/Items/ItemController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.Count - 1; i++)
+            {
+                // If available slot in inventory
+                if (inventory[i])
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Inventory/InventorySystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    public static List<InventorySlotType> inventory;
+
+    [Header("References")]
+    public CanvasGroup inventoryPopup;
+
+    private bool inventoryEnabled = false;
+
+    void Start()
+    {
+        UpdateInventoryEnabled();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            inventoryEnabled = !inventoryEnabled;
+            UpdateInventoryEnabled();
+        }
+    }
+    
+    void UpdateInventoryEnabled()
+    {
+        inventoryPopup.interactable = inventoryEnabled;
+        inventoryPopup.blocksRaycasts = inventoryEnabled;
+
+        // If the inventory is enabled
+        if (inventoryEnabled)
+        {
+            inventoryPopup.alpha = 1;
+        }
+        // If the inventory is disabled
+        else
+        {
+            inventoryPopup.alpha = 0;
+        }
+    }
+}
