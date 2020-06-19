5702ea36d2a4f0a4e8c25e23b1777d52055f5d98
Cooper Saye <coopersaye@gmail.com>
2020-06-18 23:33:56 -0700

    Implement basic item holding.

---
+++ Assets/Spickspan/Scripts/UI/Inventory/HeldItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeldItem : MonoBehaviour
+{
+    [Header("References")]
+    public Transform itemsParent;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        spriteRenderer.sprite = itemsParent.GetChild(HotbarHighlight.currentSlot).GetChild(0).GetChild(0).GetComponent<Image>().sprite;
+    }
+}
